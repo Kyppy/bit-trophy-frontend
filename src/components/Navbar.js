622348c34e0c9dfe3547f76b20eeadfed0c134e5
Kyppy Simani <kyppy.simani@andela.com>
2019-05-25 12:17:35 +0300

    Make Navbar more responsive

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Navbar(props) {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar navbar-expand-sm navbar-light bg-light">
       <span className="navbar-brand mb-0 h1">BitTrophy</span>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon" />
