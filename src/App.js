b59fb2fd64f7a4354ebb723de5ac7d31f659b30a
Kyppy Simani <kyppy.simani@andela.com>
2019-05-25 12:01:07 +0300

    Implement Navbar component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Navbar from './components/Navbar';
 
 class App extends Component {
   constructor(props) {
@@ -8,16 +9,7 @@ class App extends Component {
 
   render() {
     return (
-      <div className="dropdown">
-        <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-    Dropdown button
-        </button>
-        <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-          <a className="dropdown-item" href="#">Action</a>
-          <a className="dropdown-item" href="#">Another action</a>
-          <a className="dropdown-item" href="#">Something else here</a>
-        </div>
-      </div>
+      <Navbar />
     );
   }
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function Navbar(props) {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <span className="navbar-brand mb-0 h1">BitTrophy</span>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon" />
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div className="navbar-nav">
+          <a className="nav-item nav-link active" href="#">Trophy Room <span className="sr-only">(current)</span></a>
+          <a className="nav-item nav-link" href="#">Trophy Hall</a>
+          <a className="nav-item nav-link" href="#">Sign Out</a>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default (Navbar);
