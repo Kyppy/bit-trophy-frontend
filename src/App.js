bab77e77e3f900cd40f8e4cfe60fc5d02cdb58c3
Kyppy Simani <kyppy.simani@andela.com>
2019-05-25 15:31:28 +0300

    Add 'TrophyCase' as child component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,46 @@
 import React, { Component } from 'react';
 import Navbar from './components/Navbar';
+import TrophyCase from './components/TrophyCase';
+import { fetchGames } from './componentFunctions/trophyCaseFunctions';
+
+const Url = {
+  root: 'http://127.0.0.1:8000/api/v1/',
+};
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { rows: '', change: '' };
+    this.state = { games: [], change: '' };
+  }
+
+
+  componentDidMount() {
+    fetchGames().then((data) => {
+      console.log(data);
+      this.setState({ games: data });
+    });
   }
 
   render() {
+    const { games } = this.state;
     return (
-      <Navbar />
+      <div className="App">
+        <Navbar />
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">Title</th>
+              <th scope="col">Platform</th>
+              <th scope="col">Genre</th>
+              <th scope="col">Still Playing</th>
+              <th scope="col">Rating</th>
+            </tr>
+          </thead>
+          <tbody>
+            <TrophyCase games={games} />
+          </tbody>
+        </table>
+      </div>
     );
   }
 }
